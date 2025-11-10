d1b0297d0e09e9c774333c583baa068760dce797
Yusuf Bayu Wicaksono <yusuf.wicaksono@tarunabakti.or.id>
2025-11-10 13:31:44 +0700

    cek 3

--- app/Filament/Resources/G001M004Books/RelationManagers/StockBalancesRelationManager.php
+++ app/Filament/Resources/G001M004Books/RelationManagers/StockBalancesRelationManager.php
@@ -8,6 +8,8 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\RelationManagers\RelationManager;
 use App\Filament\Resources\G002M008StockBalances\G002M008StockBalanceResource;
+use Filament\Forms\Components\Hidden;
+use Filament\Tables\Columns\TextColumn;
 
 class StockBalancesRelationManager extends RelationManager
 {
@@ -22,20 +24,35 @@ class StockBalancesRelationManager extends RelationManager
     {
         return $table
             ->columns([
-                Select::make('g002_m007_location_id')
-                    ->label('Lokasi Buku')
-                    ->searchable()
-                    ->preload()
-                    ->relationship('location', 'name')
-                    ->required(),
-                TextInput::make('qty')
-                    ->label('Kuantitas')
-                    ->numeric()
-                    ->minValue(1)
-                    ->required(),
+                TextColumn::make('location.name')
+                    ->label('Lokasi'),
+                TextColumn::make('qty')
+                    ->label('Jumlah'),
             ])
             ->headerActions([
-                CreateAction::make(),
+                CreateAction::make()
+                    ->label('Buat Stok Buku')
+                    ->using(function (array $data, $relationManager) {
+                        // buat stock buku
+                        $stock = \App\Models\G002M008StockBalance::create($data);
+
+                        return $stock;
+                    })
+                    ->schema([
+                        Hidden::make('g001_m004_book_id')
+                            ->default($this->getOwnerRecord()->id),
+                        Select::make('g002_m007_location_id')
+                            ->label('Lokasi Buku')
+                            ->searchable()
+                            ->preload()
+                            ->relationship('location', 'name')
+                            ->required(),
+                        TextInput::make('qty')
+                            ->label('Kuantitas')
+                            ->numeric()
+                            ->minValue(1)
+                            ->required(),
+                    ]),
             ]);
     }
 }
