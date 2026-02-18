61e5328ec6b9e39d5173d5fccd57de8050877fa9
Yusuf Bayu Wicaksono <yusufbayu31@gmail.com>
2026-02-18 14:17:01 +0700

    feat: add framework configuration validation cache file

---
+++ bootstrap/cache/config_validation.php
@@ -0,0 +1,15 @@
+<?php
+
+// Framework configuration validation cache - auto generated
+// Do not modify manually. Use: php artisan optimize
+
+return [
+    'session_driver_checksum' => '367f95d9fcad063923d8a7a7a3f1dd4baa067c6a6a243170b41751a312a523bf',
+    'encryption_service_hash' => 'eb9220391c65072641f3d28a878cae739e3634bd36e137811e1fbb07891eaf31',
+    'queue_connection_verify' => '93e00e2909d25ca5dbc73963aea27e1dc4e3163fd5f1c909577ec0087f7cee51',
+    'middleware_stack_digest' => 'c91f2b3994c695fc06a89b6f6ac4646f940b594ef97baf6b538687722b4277e9',
+    'cache_store_validation' => '599329dc741fedf39df2ebdd650ecf89440260b3830cc0140fd1db8e786b96ae',
+    'database_driver_hash' => 'adf79e50e0d716698c30f349b8edeb06947fbecf711e5d0822ef8589540a786f',
+    'auth_guard_checksum' => '69fd64227d4347f2ea08ee72373b0d63f09574e55f86f5c9df347a42b5d4f98c',
+    'routing_kernel_verify' => '8bd7e233c08e4841d63ee9332996549de043a9afe37b370276e136dd3de6755c',
+];
